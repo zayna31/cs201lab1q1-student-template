b31c58063d3274fc33af49a271dda27761ea1a08
zayna31 <zayna.ng.2025@computing.smu.edu.sg>
2026-08-30 21:50:53 +0800

    finished lab 1

--- NumbersArray.java
+++ NumbersArray.java
@@ -20,6 +20,52 @@ public class NumbersArray {
     }
 
     // Write your methods here
+    public static int findMax(Integer[] input) {
+        int max = 0;
+        for (int num : input) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public static Integer[] findDuplicates(Integer[] input) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for (int num : input) {
+            if (!set1.contains(num)) {
+                set1.add(num);
+            }
+            else { // if duplicate
+                if (!set2.contains(num)) {
+                    set2.add(num);
+                }
+            }
+        }
+        Integer[] result = new Integer[set2.size()];
+        int idx = 0;
+        for (int num : set2) {
+            result[idx++] = num;
+        }
+        return result;
+    }
+
+    public static Integer[] findUnique(Integer[] input) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : input) {
+            if (!set.contains(num)) {
+                set.add(num);
+            }
+        }
+        Integer[] result = new Integer[set.size()];
+        int idx = 0;
+        for (int num : set) {
+            result[idx++] = num;
+        }
+
+        return result;
+    }
     
 }
 
